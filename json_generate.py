4d65684edf883e47063c26af573fe6ad6a1fe85e
NULL-LI <lixiang3.1416@qq.com>
2018-12-21 17:03:04 +0800

    backup for meal

--- json_generate.py
+++ json_generate.py
@@ -5,11 +5,13 @@
 
 import os
 import os.path
+import random
 import json
 import numpy as np
 import sys
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 class JsonGenerate():
     def __init__(self, root):
@@ -19,6 +21,13 @@ class JsonGenerate():
         self.basenames = []
         self.meta = {}
         self.jsonall = []
+        self.train_rate = 1
+        self.val_rate = 1
+        self.test_rate = 1
+        rate_sum = self.train_rate + self.val_rate + self.test_rate
+        self.train_rate /= rate_sum
+        self.val_rate /= rate_sum
+        self.test_rate /= rate_sum
 
     def get_file_basename(self):
         with open(self.catfile, 'r') as f:  # 读入类别名称和对应文件夹
@@ -31,16 +40,26 @@ class JsonGenerate():
             dir_point = os.path.join(self.root, self.cat[item], 'points')
             fns = sorted(os.listdir(dir_point))  # 点云文件名称
 
-
             for fn in fns:  # fns: 文件名.pts
                 token = (os.path.splitext(os.path.basename(fn))[0])
                 # self.meta[item].append((os.path.join(dir_point, token + '.pts'), os.path.join(dir_seg, token + '.seg')))
                 self.meta[item].append((os.path.join(dir_point, token + '.pts'), 'seg'))
-                self.jsonall.append(os.path.join('shape_data',self.cat[item], token))
+                self.jsonall.append(os.path.join('shape_data', self.cat[item], token))
+
     def write_json(self):
+        for file in self.jsonall:
+            rand = random.uniform(0, 1)
+            if rand<self.train_rate:#train
+                with open(os.path.join(self.root, 'train_test_split', 'shuffled_train_file_list.json'), 'r') as f:
+                    train_ids = set([str(d.split('/')[2]) for d in json.load(f)])
+            elif rand<self.train_rate+self.val_rate:#val
+                with open(os.path.join(self.root, 'train_test_split', 'shuffled_val_file_list.json'), 'r') as f:
+                    val_ids = set([str(d.split('/')[2]) for d in json.load(f)])
+            else:#test
+                with open(os.path.join(self.root, 'train_test_split', 'shuffled_test_file_list.json'), 'r') as f:
+                    test_ids = set([str(d.split('/')[2]) for d in json.load(f)])
 
 
 if __name__ == '__main__':
-    g=JsonGenerate(root = os.path.join(BASE_DIR, 'data/plan_data'))
+    g = JsonGenerate(root=os.path.join(BASE_DIR, 'data/plan_data'))
     g.get_file_basename()
-
