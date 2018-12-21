78bf2f99951d4853d0d9060a6ff7a11cf64238e9
NULL-LI <lixiang3.1416@qq.com>
2018-12-21 16:47:11 +0800

    now can read all files

--- json_generate.py
+++ json_generate.py
@@ -11,32 +11,36 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+class JsonGenerate():
+    def __init__(self, root):
+        self.root = root
+        self.catfile = os.path.join(self.root, 'synsetoffset2category.txt')
+        self.cat = {}  # 类别名称和对应文件夹
+        self.basenames = []
+        self.meta = {}
+        self.jsonall = []
 
-def get_file_basename(path):
+    def get_file_basename(self):
+        with open(self.catfile, 'r') as f:  # 读入类别名称和对应文件夹
+            for line in f:
+                ls = line.strip().split()
+                self.cat[ls[0]] = ls[1]
+
+        for item in self.cat:
+            self.meta[item] = []
+            dir_point = os.path.join(self.root, self.cat[item], 'points')
+            fns = sorted(os.listdir(dir_point))  # 点云文件名称
 
 
+            for fn in fns:  # fns: 文件名.pts
+                token = (os.path.splitext(os.path.basename(fn))[0])
+                # self.meta[item].append((os.path.join(dir_point, token + '.pts'), os.path.join(dir_seg, token + '.seg')))
+                self.meta[item].append((os.path.join(dir_point, token + '.pts'), 'seg'))
+                self.jsonall.append(os.path.join('shape_data',self.cat[item], token))
+    def write_json(self):
 
-
-
-
-def write_json(main_names):
 
 if __name__ == '__main__':
-    d = PartDataset(root=os.path.join(BASE_DIR, 'data/shapenetcore_partanno_segmentation_benchmark_v0'),
-                    class_choice=None, split='trainval')
-    print(len(d))
-    import time
-
-    tic = time.time()
-    i = 100
-    ps, seg = d[i]
-    print np.max(seg), np.min(seg)
-    print(time.time() - tic)
-    print(ps.shape, type(ps), seg.shape, type(seg))
-    sys.path.append('utils')
-    import show3d_balls
-
-    print(len(d))
-    ps, cls = d[0]
-    print(ps.shape, type(ps), cls.shape, type(cls))
+    g=JsonGenerate(root = os.path.join(BASE_DIR, 'data/plan_data'))
+    g.get_file_basename()
 
